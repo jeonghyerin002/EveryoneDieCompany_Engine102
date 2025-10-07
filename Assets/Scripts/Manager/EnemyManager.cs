4b7b682e7a0dd79ff82c18f44ea9917f769fa58c
jeonghyerin002 <jhr102446@gmail.com>
2025-10-08 01:13:12 +0900

    [Feat] 땅 수정, EnemyManager로 ㅇㄴㅁ소환은 함. 근데 이제 ㅇㄴㅁ 포지션 히얼로 와야하는데 안옴. 움직이는 코드 안써서.

--- Assets/Scripts/Manager/EnemyManager.cs
+++ Assets/Scripts/Manager/EnemyManager.cs
@@ -6,25 +6,40 @@ using UnityEngine;
 public class EnemyManager : MonoBehaviour
 {
     public EnemySO enemySO;
-    public float spawnInterval = 5f;
+    public float spawnInterval = 1f;
     public float spawnRange = 5f;
     public int maxEnemyCount;
+    public float speed = 5f;
 
-    public float timer = 0f;
+    private float timer = 0f;
+    public Transform[] here;
     void Start()
     {
-
+        
     }
-
 
     void Update()
     {
         SpawnEnemy();
+
+        for (int i = 0; i < here.Length; i++)
+        {
+            if (here[i] != null)
+            {
+                int hereIndex = i;
+                transform.position = here[i].position;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
     }
+
 
     void SpawnEnemy()
     {
-        timer = Time.deltaTime;
+        timer += Time.deltaTime;
 
         if(timer >= spawnInterval)
         {
@@ -33,9 +48,10 @@ public class EnemyManager : MonoBehaviour
                 transform.position.y,
                 transform.position.z + Random.Range(-spawnRange, spawnRange)
                 );
-
+            Debug.Log("»ý¼º");
             Instantiate(enemySO.enemyPrefab, spawnPos, Quaternion.identity);
             timer = 0f;
         }
+        
     }
 }
--- Assets/Scripts/MoveGround.cs
+++ Assets/Scripts/MoveGround.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 public class MoveGround : MonoBehaviour
 {
     public float speed = 1f;
-    // Start is called before the first frame update
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         transform.position += new Vector3(-speed * Time.deltaTime, 0.0f, 0.0f);
 
-        if(transform.position.x <= -20.0f)
+        if(transform.position.x <= 20.0f)
         {
-            transform.position += new Vector3(40.0f, 0.0f, 0.0f);
+            transform.position += new Vector3(1.0f, 0.0f, 0.0f);
         }
     }
 }
